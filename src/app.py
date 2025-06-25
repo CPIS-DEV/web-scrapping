03938652fa78af1c4d28e0a06e03907aa17fc675
CPIS-DEV <ti@cpis.com.br>
2025-06-25 16:32:39 -0300

    Perf: Tentando ativar modo headless

--- src/app.py
+++ src/app.py
@@ -118,6 +118,10 @@ def baixar_pdf(url):
 
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
     chrome_options.add_experimental_option("prefs", {
         "download.default_directory": download_dir,
         "download.prompt_for_download": False,
