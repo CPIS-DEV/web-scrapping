9187333a87f06ef5b4677bb964a64f4ac6c528ff
CPIS-DEV <ti@cpis.com.br>
2025-06-25 17:20:31 -0300

    Perf: Ajustando ouvinte do flask para todas as rotas

--- src/app.py
+++ src/app.py
@@ -137,4 +137,4 @@ def executar_busca():
         return jsonify({"status": "Nenhum resultado encontrado."})
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
